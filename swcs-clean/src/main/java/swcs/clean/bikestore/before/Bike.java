0bb64ef94caa6d8358b49b64969ee59daa7284dc
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-02-14 20:29:59 +0100

    replaced class with record

--- swcs-clean/src/main/java/swcs/clean/bikestore/before/Bike.java
+++ swcs-clean/src/main/java/swcs/clean/bikestore/before/Bike.java
@@ -1,33 +1,15 @@
 package swcs.clean.bikestore.before;
 
-public class Bike {
+public record Bike(
+        String manufacturer,
+        Bike.Type type,
+        float price
+) {
 
     public enum Type {
         MTB,
         ROAD,
         CITY,
-    }
-
-    private final Type type;
-    private final float price;
-    private final String manufacturer;
-
-    public Bike(String manufacturer, Type type, float price) {
-        this.manufacturer = manufacturer;
-        this.type = type;
-        this.price = price;
-    }
-
-    public String manufacturer() {
-        return this.manufacturer;
-    }
-
-    public Type type() {
-        return this.type;
-    }
-
-    public float price() {
-        return this.price;
     }
 
     public boolean isFullSuspension() {
